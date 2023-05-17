cabc333359e805d4b698d3bba294101b3caf0e71
ParkerLLL <parkercn@qq.com>
2023-05-17 22:21:55 +0100

    remove source code

--- src/lazy.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by parker on 17/05/2023.
- */
- 
-export default function lazy(){
-    console.log('I am lazy loading')
-}
--- src/x.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by parker on 16/05/2023.
- */
-
-import './x.css'
- 
-export default 'xxx'
